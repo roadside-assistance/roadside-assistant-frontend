dc6e44330a3fc4d4fe0548739002d97ac5252a5a
Parsa <mo.rostami@digikala.com>
2021-08-22 22:50:09 +0430

    added report.

--- src/views/Reports.js
+++ src/views/Reports.js
@@ -32,120 +32,151 @@ import CustomFileUpload from "../components/components-overview/CustomFileUpload
 import DropdownInputGroups from "../components/components-overview/DropdownInputGroups";
 import CustomSelect from "../components/components-overview/CustomSelect";
 
-const ComponentsOverview = () => (
-    <div>
-        <Container fluid className="main-content-container px-4">
-            <Row noGutters className="page-header py-4">
-                <PageTitle
-                    sm="4"
-                    title="State Manager Report"
-                    subtitle="Report"
-                    className="text-sm-left"
-                />
-            </Row>
+class ComponentsOverview extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            items: []
+        }
+    }
 
-            <Row>
-                <Col lg="8" className="mb-4">
-                    <Card small className="mb-4">
-                        <CardHeader className="border-bottom">
-                            <h6 className="m-0">Report</h6>
-                        </CardHeader>
-                        <CardBody className="p-0 pb-3">
-                            <table className="table mb-0">
-                                <thead className="bg-light">
-                                <tr>
-                                    <th scope="col" className="border-0">
-                                        #
-                                    </th>
-                                    <th scope="col" className="border-0">
-                                        First Name
-                                    </th>
-                                    <th scope="col" className="border-0">
-                                        Last Name
-                                    </th>
-                                    <th scope="col" className="border-0">
-                                        Country
-                                    </th>
-                                    <th scope="col" className="border-0">
-                                        City
-                                    </th>
-                                    <th scope="col" className="border-0">
-                                        Phone
-                                    </th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>1</td>
-                                    <td>Ali</td>
-                                    <td>Kerry</td>
-                                    <td>Russian Federation</td>
-                                    <td>Gdańsk</td>
-                                    <td>107-0339</td>
-                                </tr>
-                                <tr>
-                                    <td>2</td>
-                                    <td>Clark</td>
-                                    <td>Angela</td>
-                                    <td>Estonia</td>
-                                    <td>Borghetto di Vara</td>
-                                    <td>1-660-850-1647</td>
-                                </tr>
-                                <tr>
-                                    <td>3</td>
-                                    <td>Jerry</td>
-                                    <td>Nathan</td>
-                                    <td>Cyprus</td>
-                                    <td>Braunau am Inn</td>
-                                    <td>214-4225</td>
-                                </tr>
-                                <tr>
-                                    <td>4</td>
-                                    <td>Colt</td>
-                                    <td>Angela</td>
-                                    <td>Liberia</td>
-                                    <td>Bad Hersfeld</td>
-                                    <td>1-848-473-7416</td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </CardBody>
-                    </Card>
-                </Col>
+    componentWillMount() {
+        fetch('http://207.182.143.36/:8080/problem-solving/problems/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            mode : 'cors'
+        })
+            .then(res => {
+                return res.json()
+            })
+            .then(json => {
+                this.setState({
+                    isLoaded: true,
+                    items: json
+                })
+                console.log(this.state.items)
+            })
+    }
 
-                <Col lg="4" className="mb-4">
+    render() {
 
-                    <Card small>
-                        {/* Files & Dropdowns */}
-                        <CardHeader className="border-bottom">
-                            <h6 className="m-0">Report Options</h6>
-                        </CardHeader>
+        var {isLoaded, items} = this.state
+        if (!isLoaded) {
+            return <div>
+                loading ...
+            </div>
+        } else {
+            return (
+                <div>
+                    <Container fluid className="main-content-container px-4">
+                        <Row noGutters className="page-header py-4">
+                            <PageTitle
+                                sm="4"
+                                title="State Manager Report"
+                                subtitle="Report"
+                                className="text-sm-left"
+                            />
+                        </Row>
 
-                        <ListGroup flush>
-                            <ListGroupItem className="px-3">
-                                <strong className="text-muted d-block mb-2">
-                                    Custom Select
-                                </strong>
-                                <div>
-                                    <InputGroup className="mb-3">
-                                        <FormSelect>
-                                            <option>Choose</option>
-                                            <option>Machinery</option>
-                                            <option>Human Resources</option>
-                                            <option>Problems</option>
-                                        </FormSelect>
-                                        <InputGroupAddon type="append">
-                                            <InputGroupText>Options</InputGroupText>
-                                        </InputGroupAddon>
-                                    </InputGroup>
-                                </div>
-                            </ListGroupItem>
-                        </ListGroup>
-                    </Card>
-                </Col>
-            </Row>
-        </Container>
-    </div>
-);
+                        <Row>
+                            <Col lg="8" className="mb-4">
+                                <Card small className="mb-4">
+                                    <CardHeader className="border-bottom">
+                                        <h6 className="m-0">Report</h6>
+                                    </CardHeader>
+                                    <CardBody className="p-0 pb-3">
+                                        <table className="table mb-0">
+                                            <thead className="bg-light">
+                                            <tr>
+                                                <th scope="col" className="border-0">
+                                                    #
+                                                </th>
+                                                <th scope="col" className="border-0">
+                                                    Address
+                                                </th>
+                                                <th scope="col" className="border-0">
+                                                    Description
+                                                </th>
+                                                <th scope="col" className="border-0">
+                                                    Status
+                                                </th>
+                                                <th scope="col" className="border-0">
+                                                    # of needed workers
+                                                </th>
+                                                <th scope="col" className="border-0">
+                                                    Inspector Id
+                                                </th>
+                                                <th scope="col" className="border-0">
+                                                    Team Id
+                                                </th>
+                                                <th scope="col" className="border-0">
+                                                    Issuer Id
+                                                </th>
+                                            </tr>
+                                            </thead>
+                                            <tbody>
+                                            {
+                                                items.map(
+                                                    item => (
+                                                        <tr>
+                                                            <td>{item.id}</td>
+                                                            <td>{item.address}</td>
+                                                            <td>{item.description}</td>
+                                                            <td>{item.status}</td>
+                                                            <td>{item.number_of_needed_workers}</td>
+                                                            <td>{item.assigned_to_inspector}</td>
+                                                            <td>{item.assigned_to_team}</td>
+                                                            <td>{item.issuer}</td>
+                                                        </tr>
+                                                    )
+                                                )
+                                            }
+
+                                            </tbody>
+                                        </table>
+                                    </CardBody>
+                                </Card>
+                            </Col>
+
+                            <Col lg="4" className="mb-4">
+
+                                <Card small>
+                                    {/* Files & Dropdowns */}
+                                    <CardHeader className="border-bottom">
+                                        <h6 className="m-0">Report Options</h6>
+                                    </CardHeader>
+
+                                    <ListGroup flush>
+                                        <ListGroupItem className="px-3">
+                                            <strong className="text-muted d-block mb-2">
+                                                Custom Select
+                                            </strong>
+                                            <div>
+                                                <InputGroup className="mb-3">
+                                                    <FormSelect>
+                                                        <option>Choose</option>
+                                                        <option>Machinery</option>
+                                                        <option>Human Resources</option>
+                                                        <option>Problems</option>
+                                                    </FormSelect>
+                                                    <InputGroupAddon type="append">
+                                                        <InputGroupText>Options</InputGroupText>
+                                                    </InputGroupAddon>
+                                                </InputGroup>
+                                            </div>
+                                        </ListGroupItem>
+                                    </ListGroup>
+                                </Card>
+                            </Col>
+                        </Row>
+                    </Container>
+                </div>
+            );
+        }
+    }
+}
 
 export default ComponentsOverview;
